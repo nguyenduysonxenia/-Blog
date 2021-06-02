1cddc7b7a8656595a31bc3a35c36d450e33a6099
nguyen duy son <nguyenduyson60@gmail.com>
2021-06-02 22:26:45 +0700

    like and view

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -3,6 +3,9 @@ import PostController from "../controllers/PostController";
 import checkLogin from "../middlewares/checkLogin";
 const router = express.Router();
 router.use(checkLogin);
+router.get('/search',PostController.search);
+router.patch('/:id/like',PostController.like)
+router.patch('/:id/view',PostController.view)
 router.get("/", PostController.getPostList);
 router.post("/", PostController.createPost);
 router.get("/:id", PostController.findOnePost);
