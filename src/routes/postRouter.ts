7f30acbfaa63232aa8a9d315202be1720fb94217
nguyen duy son <nguyenduyson60@gmail.com>
2021-06-05 22:10:41 +0700

    fix post

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import PostController from "../controllers/PostController";
 import checkLogin from "../middlewares/checkLogin";
+import multer from '../utils/multer';
 const router = express.Router();
 router.use(checkLogin);
 router.get('/search',PostController.search);
 router.patch('/:id/like',PostController.like)
 router.patch('/:id/view',PostController.view)
 router.get("/", PostController.getPostList);
-router.post("/", PostController.createPost);
+router.post("/", multer.single('image'),PostController.createPost);
 router.get("/:id", PostController.findOnePost);
-router.patch("/:id", PostController.updatePost);
+router.patch("/:id", multer.single('image'),PostController.updatePost);
 router.delete("/:id", PostController.deletePost);
 
 export default router;
