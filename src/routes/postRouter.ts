12f770d1be1a4040e5ea774014f287f9167f5cdb
nguyen duy son <nguyenduyson60@gmail.com>
2021-06-07 15:03:35 +0700

    fix post 4

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -6,11 +6,10 @@ import Post from '../models/post';
 import multer from '../utils/multer';
 
 const router = express.Router();
-
-router.get('/search',PostController.search);
-router.get("/:id", PostController.findOnePost);
 router.get('/new',PostController.getPostNew);
 router.get('/hot',PostController.getPostHot);
+router.get('/search',PostController.search);
+router.get("/:id", PostController.findOnePost);
 router.patch('/:id/like',PostController.like);
 router.patch('/:id/view',PostController.view);
 //authen
